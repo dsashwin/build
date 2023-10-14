af28062917a5931114fb201a04aa45db5891ba26
jhenrique09 <jhsv09@gmail.com>
2023-10-14 18:13:24 -0300

    build: Changes for PixelOS
    Change-Id: I3137f087c24de637f471f991b837482d9d79ae3f

--- core/config.mk
+++ core/config.mk
@@ -407,8 +407,8 @@ endif
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
-ifneq ($(LINEAGE_BUILD),)
-include vendor/lineage/config/BoardConfigLineage.mk
+ifneq ($(CUSTOM_BUILD),)
+include vendor/aosp/config/BoardConfig.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1297,7 +1297,7 @@ dont_bother_goals := out product-graph
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
-ifneq ($(LINEAGE_BUILD),)
+ifneq ($(CUSTOM_BUILD),)
 ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
@@ -1316,6 +1316,6 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
 # Include any vendor specific config.mk file
--include vendor/*/build/core/config.mk
+-include vendor/aosp/build/core/config.mk
 
 include $(BUILD_SYSTEM)/dumpvar.mk
